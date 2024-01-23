b8800cd3b10a36b5ca6cf87897e49ceb174d2d2c
Jordan Munoz <jordanmunoz@Jordans-MacBook-Air.local>
2024-01-22 20:35:01 -0600

    created a functioning register component and began work on login component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,1 +1,98 @@
-/* TODO - add your code to create a functional React component that renders a registration form */
+import { useState } from "react";
+
+const Register = ({ setToken }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [validationError, setValidationError] = useState("");
+
+  const validateForm = () => {
+    if (password.length < 8) {
+      setValidationError("Password must be at least 8 characters long");
+      return false;
+    }
+    setValidationError("");
+    return true;
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setEmail("");
+    setFirstName("");
+    setLastName("");
+    setPassword("");
+
+    if (!validateForm()) {
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            password,
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+      setToken(result.token);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Register Now!</h2>
+      {validationError && <p>{validationError}</p>}
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name (optional):{" "}
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name (optional):{" "}
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:{" "}
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;

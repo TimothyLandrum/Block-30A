35e21e05fcdfb52e25963a801e8ee185753d7935
Jordan Munoz <jordanmunioz@gmail.com>
2024-01-28 23:59:05 -0600

    changed site name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -64,7 +64,7 @@ function NavBar({token, setToken}) {
               textDecoration: "none",
             }}
           >
-            Library Express
+            Book Buddy
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
